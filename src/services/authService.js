2a03d14b4e4a22a75eb8b4f05ead42295fc2a11a
KALLURI YASWANTH <143748234+yaswanth5019k@users.noreply.github.com>
2025-08-21 17:52:59 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:9090/back1/auth";
+const API_URL = "http://localhost:9191/ecommerce-1.0.0/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
