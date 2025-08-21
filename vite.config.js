eacd6c2f0240d064d4f66ff2a75f62f870af3a45
yaswanth5019k <yaswanthkalluri2005@gmail.com>
2025-08-21 23:34:05 +0530

    changed vite.config page target port 9191

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/ecommerce/',
+  server: {
+    proxy: {
+      // Proxy API requests starting with /auth to your backend
+      '/auth': {
+        target: 'http://localhost:9191',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
